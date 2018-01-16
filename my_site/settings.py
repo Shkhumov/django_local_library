c217d52a9e6a3055f278a09a2946971cd5360625
Orest <orest.sooz@gmail.com>
2018-01-16 16:30:57 +0200

    Added a Procfile.

--- my_site/settings.py
+++ my_site/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
   #     SECRET_KEY = open(SECRET_FILE).read().strip()
    # except IOError:
     #    SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 #DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
